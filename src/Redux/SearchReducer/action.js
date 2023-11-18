58c6cce860b13eda478e5948a84739dd49292d82
Jitu gandhare <jitugandhare@gmail.com>
2023-11-18 17:35:02 +0530

    payment upi functionallity done

--- src/Redux/SearchReducer/action.js
+++ src/Redux/SearchReducer/action.js
@@ -1,21 +1,43 @@
+// import axios from "axios";
+// import { SEARCH_FAILURE, SEARCH_REQUEST, SEARCH_SUCCESS } from "./actionTypes"
+
+
+// export const getSearch = (val) => (dispatch) =>{
+//     dispatch({type: SEARCH_REQUEST});
+//     axios.get(`https://productdecisiveduck.onrender.com/products`, {
+//       params: {
+//         _limit: 6,
+//         _page: 1,
+//         q: val
+//       }
+//     })
+//     .then((res)=>{
+//         console.log(res.data)
+//         dispatch({type:SEARCH_SUCCESS, payload: res.data});
+//     })
+//     .catch((err)=>{
+//         dispatch({type: SEARCH_FAILURE})
+//     })
+// }
+
 import axios from "axios";
-import { SEARCH_FAILURE, SEARCH_REQUEST, SEARCH_SUCCESS } from "./actionTypes"
+import { SEARCH_FAILURE, SEARCH_REQUEST, SEARCH_SUCCESS } from "./actionTypes";
 
-
-export const getSearch = (val) => (dispatch) =>{
-    dispatch({type: SEARCH_REQUEST});
-    axios.get(`https://productdecisiveduck.onrender.com/products`, {
+export const getSearch = (val) => (dispatch) => {
+  dispatch({ type: SEARCH_REQUEST });
+  axios
+    .get(`https://productdecisiveduck.onrender.com/products`, {
       params: {
         _limit: 6,
         _page: 1,
-        q: val
-      }
+        q: val,
+      },
     })
-    .then((res)=>{
-        console.log(res.data)
-        dispatch({type:SEARCH_SUCCESS, payload: res.data});
+    .then((res) => {
+      console.log(res.data);
+      dispatch({ type: SEARCH_SUCCESS, payload: res.data });
     })
-    .catch((err)=>{
-        dispatch({type: SEARCH_FAILURE})
-    })
-}
+    .catch((err) => {
+      dispatch({ type: SEARCH_FAILURE });
+    });
+};
